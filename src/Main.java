d70219073e7952cf94a593f1e405daefb940791b
Abhirami155 <as8769@srmist.edu.in>
2026-03-10 18:51:18 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("PALINDROME CHECKER APP");
+        System.out.println("Version 1.0");
+        System.out.println("Welcome to Palindrome Checker Application");
+
+    }
+}
